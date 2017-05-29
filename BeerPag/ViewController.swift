c995d8917722dcc7612783c6d87fe49793c789e4
Ricardo Bailoni <rbailoni@biolabfarma.com.br>
2017-05-29 10:31:14 -0300

    Initial Commit

---
+++ BeerPag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerPag
+//
+//  Created by Ricardo do Espirito Santo Bailoni on 29/05/17.
+//  Copyright © 2017 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
